3cededbc99d9d64238e50d2cb723e969105b86a9
Orhan Uckulac <=>
2022-10-25 05:53:44 +0300

    third commit - ask gallery permission to upload an image to draw on add , small syntax changes

--- app/src/main/java/orhan/uckulac/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/orhan/uckulac/kidsdrawingapp/MainActivity.kt
@@ -1,12 +1,19 @@
 package orhan.uckulac.kidsdrawingapp
 
+import android.Manifest
 import android.app.Dialog
-import android.media.Image
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import orhan.uckulac.kidsdrawingapp.databinding.ActivityMainBinding
@@ -16,7 +23,53 @@ class MainActivity : AppCompatActivity() {
     private var drawingView: DrawingView? = null
     private var mImageButtonCurrentPaint: ImageButton? = null
 
-    var brushSizeBtn: ImageButton? = null
+    // create a result launcher to get the image URI from the phone gallery
+    // first define what kind of a launcher will it be? 'intent'
+    // register for activity result to get result
+    private val openGalleryLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        result ->
+            // check the result, if It's okay and if the result data is not empty,
+            // then get the location of the data, URI, and assign it as background image
+            if (result.resultCode == RESULT_OK && result.data != null){
+                val imageBackground: ImageView = binding.ivBackground
+                imageBackground.setImageURI(result.data?.data)
+            }
+    }
+
+    private val requestPermission: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            permissions.entries.forEach {
+                val perMissionName = it.key
+                val isGranted = it.value
+                // if permission is granted show a toast and perform operation
+                if (isGranted ) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Permission granted now you can read the storage files.",
+                        Toast.LENGTH_LONG
+                    ).show()
+
+                    // start an intent to access Media in the phone
+                    val pickIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    openGalleryLauncher.launch(pickIntent)
+
+
+                    //perform operation
+                } else {
+                    // Displaying another toast if permission is not granted and this time focus on
+                    //    Read external storage
+                    if (perMissionName == Manifest.permission.READ_EXTERNAL_STORAGE)
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Oops you just denied the permission.",
+                            Toast.LENGTH_LONG
+                        ).show()
+                }
+            }
+
+        }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,10 +82,14 @@ class MainActivity : AppCompatActivity() {
         val linearLayoutPaintColors = binding.llPaintColors  // entire linear layout that contains all the colors
         mImageButtonCurrentPaint = linearLayoutPaintColors[1] as ImageButton  // treat like an image button
 
-
-        brushSizeBtn = binding.ibBrush
-        brushSizeBtn?.setOnClickListener {
+        val brushSizeBtn = binding.ibBrush
+        brushSizeBtn.setOnClickListener {
             brushSizeChooserDialog()
+        }
+
+        val uploadImage = binding.ibGallery
+        uploadImage.setOnClickListener {
+            requestStoragePermission()
         }
     }
 
@@ -41,17 +98,17 @@ class MainActivity : AppCompatActivity() {
         val brushDialog = Dialog(this)
         brushDialog.setContentView(R.layout.dialog_brush_size)
         brushDialog.setTitle("Brush siz: ")
-        val smallBtn = brushDialog.findViewById<ImageButton>(R.id.ib_small_brush)
+        val smallBtn = brushDialog.findViewById<ImageButton>(R.id.ibSmallBrush)
         smallBtn.setOnClickListener{
             drawingView?.setSizeForBrush(10.toFloat())
             brushDialog.dismiss()
         }
-        val mediumBtn = brushDialog.findViewById<ImageButton>(R.id.ib_medium_brush)
+        val mediumBtn = brushDialog.findViewById<ImageButton>(R.id.ibMediumBrush)
         mediumBtn.setOnClickListener{
             drawingView?.setSizeForBrush(20.toFloat())
             brushDialog.dismiss()
         }
-        val largeBtn = brushDialog.findViewById<ImageButton>(R.id.ib_large_brush)
+        val largeBtn = brushDialog.findViewById<ImageButton>(R.id.ibLargeBrush)
         largeBtn.setOnClickListener {
             drawingView?.setSizeForBrush(30.toFloat())
             brushDialog.dismiss()
@@ -78,4 +135,43 @@ class MainActivity : AppCompatActivity() {
 
         }
     }
+
+    private fun requestStoragePermission(){
+        //Check if the permission was denied and show rationale
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && shouldShowRequestPermissionRationale(
+                Manifest.permission.CAMERA)
+        ){
+            //call the rationale dialog to tell the user why they need to allow permission request
+            showRationaleDialog("Kids Drawing App","Kids Drawing App " +
+                    "needs to Access Your External Storage")
+        }
+        else {
+            // You can directly ask for the permission.
+            // if it has not been denied then request for permission
+            // The registered ActivityResultCallback gets the result of this request.
+            requestPermission.launch(
+                arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                )
+            )
+        }
+    }
+
+    /** create rationale dialog
+     * Shows rationale dialog for displaying why the app needs permission
+     * Only shown if the user has denied the permission request previously
+     */
+    private fun showRationaleDialog(
+        title: String,
+        message: String,
+    ){
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("Cancel"){
+                    dialog, _-> dialog.dismiss()
+            }
+        builder.create().show()
+    }
+
 }
